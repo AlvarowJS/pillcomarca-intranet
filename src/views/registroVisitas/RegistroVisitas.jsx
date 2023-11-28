d6b7eaef0ba0b0f25f5caf7e41b607734c70c56d
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2023-11-28 15:19:06 -0500

    subiendo cambios para hora

--- src/views/registroVisitas/RegistroVisitas.jsx
+++ src/views/registroVisitas/RegistroVisitas.jsx
@@ -37,7 +37,7 @@ const RegistroVisitas = () => {
         dni: '',
         persona: '',
         asunto: '',
-        hora_ingreso: '',
+        // hora_ingreso: '',
         hora_salida: ''
     }
 
