961efccc2786522c21bd62489601b21b5c617876
Anthony <thonyrivera9@gmail.com>
2024-02-16 12:52:27 -0500

    avance de documento normativa

--- src/views/documentosNormativa/DocumentosNormativa.jsx
+++ src/views/documentosNormativa/DocumentosNormativa.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import TablaNormativa from './TablaNormativa'
+import bdMuni from '../../api/bdMuni';
+
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+const MySwal = withReactContent(Swal)
+const URL = '/v1/documentonormativa'
 
 const DocumentosNormativa = () => {
+  const token = localStorage.getItem('token')
+  const [data, setData] = useState()
+  const getAuthHeaders = () => ({
+    headers: {
+        'Authorization': 'Bearer ' + token
+    }
+  });
+  useEffect(() => {
+    bdMuni.get(URL, getAuthHeaders())
+            .then(res => {
+                setData(res.data.data)
+            })
+            .catch(err => {
+
+            })
+  }, [])
+
+
   return (
-    <div>DocumentosNormativa</div>
+    <>
+      <TablaNormativa 
+      data={data}
+      />
+      {/* <FormNormativa /> */}
+
+    </>
   )
 }
 
